61d89c385a44751fe61dca842eb7d2e05e7e7a6c
tinabeattysmith <tinatest2020@gmail.com>
2020-10-25 23:37:10 -0400

    Updated Login.  Assignment INPRG

--- src/auth/Login.js
+++ src/auth/Login.js
@@ -6,18 +6,34 @@ const Login = (props) => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
+    let handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(username,password);
+        fetch("http://localhost:3000/user/login", {
+          method: 'POST',
+          body: JSON.stringify({user:{username: username, password: password}}), 
+          headers: new Headers({
+              'Content-Type': 'application/json'
+          })
+        }).then(
+            (response) => response.json()
+        ).then((data) => {
+            props.updateToken(data.sessionToken)
+        });
+    };
+
     return(
         <div>
             <h1>Login</h1>
-            <Form>
+            <Form onSubmit={handleSubmit}>
                 <FormGroup>
-                    {/* Input fields are tied the state variables, which currently never change, their text content will be static as well (in this case it will be empty). */}
+                    {/* Input fields are tied the state variables */}
                     <Label htmlFor="username">Username</Label>
-                    <Input name="username" value={username}/>
+                    <Input onChange={(e) => setUsername(e.target.value)} name="username" value={username}/>
                 </FormGroup>
                 <FormGroup>
                     <Label htmlFor="password">Password</Label>
-                    <Input name="password" value={password}/>
+                    <Input onChange={(e) => setPassword(e.target.value)} name="password" value={password}/>
                 </FormGroup>
                 <Button type = "submit">Login</Button>
             </Form>
